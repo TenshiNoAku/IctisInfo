52f08d9e3139d9f0e6372f5d6b60a7a88e65cdfa
unknown <tenshinoakuma@mail.ru>
2024-06-30 22:55:03 +0300

    Schedule remove test

--- src/Controller/ScheduleController.php
+++ src/Controller/ScheduleController.php
@@ -24,63 +24,6 @@ class ScheduleController extends AbstractController
     }
     
         
-    
-    
-    #[Route('/test', name: 'test', methods:['get'])]
-    public function test(HttpClientInterface $client): JsonResponse
-    {	
-    
-    	$audience_name_files = [];
-        $audience_info = $client->request('GET','https://webictis.sfedu.ru/schedule-api/?query=%D0%93-4')->toArray()["choices"];
-        foreach($audience_info as $info){
-        	$audience_name_files[$info['name']] = $info['group'];
-
-        }
-        $date = new \DateTime('05-02-2024');
-        $weeks = $client->request('GET',"https://webictis.sfedu.ru/schedule-api/?group=a18.html&week=5")->toArray()["weeks"];
-        $group = "a26.html";
-        $classes_name_desc = [];
-        $schedule = $client->request('GET',"https://webictis.sfedu.ru/schedule-api/?group=a26.html&week=1")->toArray()["table"]["table"];
-        foreach ($weeks as $week){
-        $schedule = $client->request('GET',"https://webictis.sfedu.ru/schedule-api/?group={$group}&week={$week}")->toArray()["table"]["table"];
-        $class_time = array_slice($schedule[1],1);
-        $schedule_classes = array_slice($schedule,2);
-        foreach ($schedule_classes as $classes){
-        	$classes = array_slice($classes,1);
-        	$class_num = 0;
-        	foreach ($classes as $class){
-        		
-        		$class_name = "";
-        		$description= "";
-        		$flag = 0;
-        		$class = explode(" ", $class);
-        		for ($i = 0; $i < count($class);$i++){
-        			if ($flag == 1) {$description.=$class[$i] . " ";}
-				else if (!str_contains($class[$i], "пр.") and !str_contains($class[$i], "экз.") and !str_contains($class[$i], "лаб.")){
-					$class_name .= $class[$i] . " ";
-				}
-				else{
-					$description .= $class[$i] . " ";
-					$flag = 1;
-				}
-							
-			}
-			
-			$classes_name_desc[] = ["name"=>$class_name , "desc"=>$description,"date"=>$date->format("d-m-Y")];
-			
-		
-        	}
-        	
-        	$date->modify("+ 1 day");
-        }}
-        
-        return $this->json([
-        	$classes_name_desc,
-        	$audience_name_files,
-        	$class_time,
-    
-        ]);
-    }
 
 
     #[Route('/schedules', name: 'schedules_create', methods:['post'] )]
